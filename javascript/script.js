d335796c31f846f1c021a40e368f9c7f609bb6c9
Salih <salihyakuphankeskin@gmail.com>
2022-03-02 20:57:45 +0300

    master

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,27 @@
 'use strict';
+fetch('/JSON/01.json')
+.then(results => results.json())
+.then(data => {
+    console.log(data.name)
+    document.querySelector("#first").innerText = data.name
+})
+const meJSON = `
+    {
+        "name": "Salih",
+        "age": 21,
+        "graduated": false,
+        "favoriteColor": [
+            "violetta",
+            "magenta"
+        ]
+    }
+`;
+const me = JSON.parse(meJSON);
+console.log(me);
+
+const car = {
+    make: "Honda",
+    model: "Civic"
+};
+const carJson = JSON.stringify(car);
+console.log(carJson);
