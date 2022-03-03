55b53caab746bab5ff418a35e4880d7376a57ace
Salih <salihyakuphankeskin@gmail.com>
2022-03-03 12:29:19 +0300

    master

--- javascript/script.js
+++ javascript/script.js
@@ -21,13 +21,10 @@ const car = {
 const carJson = JSON.stringify(car);
 console.log(carJson);
 // --------------------------------------------------
-fetch('/JSON/people.json').then(function(response){
-    return response.json();
-}).then(function(obj){
-    console.log(obj);
-}).catch(function(error){
-    console.error('ERROR WARNING')
-})
+fetch('/JSON/people.json')
+.then(function(response){return response.json();})
+.then(function(obj){console.log(obj);})
+.catch(function(error){console.error('ERROR WARNING')})
 
 document.getElementById("div-to-be-changed").textContent = carJson;
 function changeText(){
@@ -36,5 +33,27 @@ function changeText(){
     document.getElementById("div-to-be-changed").textContent = document.getElementById("written-text").value;
     // document.getElementById("div-to-be-changed").textContent = "";    
 }
-// var obj01 = require('/JSON/people.json');
-// console.log(obj01);
+fetch('/JSON/people02.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+fetch('/JSON/people02.json').then(abc => console.log(abc));
+  
+fetch('/JSON/people02.json')
+.then(abcd => abcd.json())
+.then(abcde => console.log(abcde));
+
+let xaybc= `
+    {
+        "name": "Salih",
+        "age": 21,
+        "graduated": false,
+        "favoriteColor": [
+            "violetta",
+            "magenta"
+        ]
+    }
+`;
+let xaybcKSON= JSON.parse(xaybc);
+console.log(xaybc);
+console.log(xaybcKSON);
